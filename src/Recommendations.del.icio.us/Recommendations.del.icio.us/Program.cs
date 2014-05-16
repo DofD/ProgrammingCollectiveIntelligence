e3a7d3ac480526710c1b3542155692ef2905ad67
DofD <DemonOfDarkness@mail.ru>
2014-05-16 13:47:19 +0000

    + Добавлена заготовка проекта для анализа del.icio.us

---
+++ src/Recommendations.del.icio.us/Recommendations.del.icio.us/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Recommendations.del.icio.us
+{
+    class Program
+    {
+        // http://previous.delicious.com/v2/rss/popular/programming
+        // https://github.com/zobie/DeliciousNet
+    }
+}
